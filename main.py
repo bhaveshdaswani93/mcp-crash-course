cfc1e51d927d2a931beb3fb485959f25023d01b3
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2025-10-23 08:30:57 +0000

    Add initial project structure with Python environment setup
    - Created .gitignore to exclude Python artifacts and environment files.
    - Added .python-version to specify Python version 3.13.
    - Implemented main.py with basic asynchronous functionality and environment loading.
    - Defined math_server.py and weather_server.py with MCP tools for addition, multiplication, and weather retrieval.
    - Included pyproject.toml for project metadata and dependencies.
    - Added uv.lock for dependency management.

---
+++ main.py
@@ -0,0 +1,27 @@
+import asyncio
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+load_dotenv()
+
+llm = ChatOpenAI()
+print(Path("servers/math_server.py").absolute().as_posix())
+
+stdio_server_params =  StdioServerParameters(
+    command="python",
+    args=[Path("servers/math_server.py").absolute().as_posix()],
+)
+
+async def main():
+    print("Hello from mcp-crash-course!")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
